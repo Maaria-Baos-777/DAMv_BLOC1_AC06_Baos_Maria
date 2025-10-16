25e135457d9278b052385a3a93c6213beb189bc8
Maaria-Baos-777 <maria.baos.7e9@itb.cat>
2025-10-16 13:04:40 +0200

    Solutions Exercici2

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+namespace AC06CelsiusToKelvin
+{
+    public class CelsiusToKelvin
+    {
+        public static void Main()
+        {
+            const string MSG_INPUT = "Enter tempature in Celsius:";
+            const string MSG_RESULT = "Temperature in Kelvin:";
+
+            double celsius;
+            double kelvin;
+
+            Console.WriteLine(MSG_INPUT);
+            celsius = Convert.ToDouble(Console.ReadLine());
+
+            kelvin = celsius + 273.15;
+
+            Console.WriteLine(MSG_RESULT + kelvin);
+        }
+    }
+}
+
+
